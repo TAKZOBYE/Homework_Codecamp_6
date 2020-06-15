cbc373d11784dac54878f51083bf43a1be224ae1
TAKZOBYE <takerchanel@gmail.com>
2020-06-15 12:08:03 +0700

    Add 3 Exercise

---
+++ JS/Video 3 Exercise 6/script.js
@@ -0,0 +1,13 @@
+let point = prompt('กรอกคะแนนของคุณ')
+
+if (point >= 80) {
+    alert('A')
+} else if (point >= 70 && point <= 79) {
+    alert('B')
+} else if (point >= 60 && point <= 69) {
+    alert('C')
+} else if (point >= 50 && point <= 59) {
+    alert('D')
+} else if (point < 50) {
+    alert('F')
+}
